7f3860a6cac29a832162081a8254ba80784d26d9
Chemik <dj-chemik@wp.pl>
2020-09-17 23:05:02 +0200

    Adjust basic screens

--- app/src/main/java/pl/chemik/ubiapp/activities/StartActivity.kt
+++ app/src/main/java/pl/chemik/ubiapp/activities/StartActivity.kt
@@ -3,9 +3,7 @@ package pl.chemik.ubiapp.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.View
-import android.widget.EditText
 import pl.chemik.ubiapp.R
 
 class StartActivity : AppCompatActivity() {
@@ -13,15 +11,24 @@ class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //sendMessage()
     }
 
-    fun sendMessage(view: View) {
-        //val editText = findViewById<EditText>(R.id.editText)
-        val message = "HEJO" //editText.text.toString()
-        val intent = Intent(this, LocationsListActivity::class.java).apply {
-            putExtra(EXTRA_MESSAGE, message)
-        }
-        startActivity(intent)
+    fun clickGoToLocationsList(view: View) {
+        val intent = Intent(this, LocationsListActivity::class.java);
+        startActivity(intent);
+    }
+
+    fun clickGoToItemsList(view: View) {
+        val intent = Intent(this, ItemsListActivity::class.java);
+        startActivity(intent);
+    }
+
+    fun clickGoToSettings(view: View) {
+        val intent = Intent(this, SettingsActivity::class.java);
+        startActivity(intent);
+    }
+
+    fun clickExit(view: View) {
+
     }
 }
