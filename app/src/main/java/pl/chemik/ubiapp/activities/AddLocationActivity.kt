277fab0d83c26bfd74afcb096c517969071dc0c6
Chemik <dj-chemik@wp.pl>
2020-09-21 21:08:39 +0200

    Add AddLocation button

---
+++ app/src/main/java/pl/chemik/ubiapp/activities/AddLocationActivity.kt
@@ -0,0 +1,12 @@
+package pl.chemik.ubiapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pl.chemik.ubiapp.R
+
+class AddLocationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_location)
+    }
+}
