17490798dc0bf6583c49ddb56ac8fad4d91b7197
Chemik <dj-chemik@wp.pl>
2020-09-30 22:18:13 +0200

    Add searching in items activity

--- app/src/main/java/pl/chemik/ubiapp/activities/ItemAdapter.kt
+++ app/src/main/java/pl/chemik/ubiapp/activities/ItemAdapter.kt
@@ -1,13 +1,18 @@
 package pl.chemik.ubiapp.activities
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import pl.chemik.ubiapp.R
 import pl.chemik.ubiapp.database.entities.Item
+import java.util.*
+import kotlin.collections.ArrayList
 
 interface ItemClickListener {
     fun onItemClick(view: View, position: Int);
@@ -18,11 +23,13 @@ interface RecycledListItemClickListener {
 }
 
 class ItemAdapter(context: Context, items: List<Item>) :
-    RecyclerView.Adapter<ItemAdapter.ItemViewHolder>(), ItemClickListener {
+    RecyclerView.Adapter<ItemAdapter.ItemViewHolder>(), ItemClickListener, Filterable {
 
     val items: List<Item> = items;
+    var itemFilterList: List<Item> = items;
     val context: Context = context;
     private var listener: RecycledListItemClickListener? = null
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemAdapter.ItemViewHolder {
         val inflater = LayoutInflater.from(context);
@@ -32,12 +39,70 @@ class ItemAdapter(context: Context, items: List<Item>) :
         return viewHolder;
     }
 
+    fun filterByBox(boxId: Int): Filter {
+
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                if (boxId == -1) {
+                    Log.e("---", "-1")
+                    itemFilterList = items
+                } else {
+                    Log.d("----", boxId.toString())
+                    val resultList = ArrayList<Item>()
+                    for (row in items) {
+                        if (row.boxId == boxId) {
+                            resultList.add(row)
+                        }
+                    }
+                    itemFilterList = resultList
+                }
+                val filterResults = FilterResults()
+                filterResults.values = itemFilterList
+                return filterResults
+            }
+
+            @Suppress("UNCHECKED_CAST")
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                itemFilterList = results?.values as ArrayList<Item>
+                notifyDataSetChanged()
+            }
+        };
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val charSearch = constraint.toString()
+                if (charSearch.isEmpty()) {
+                    itemFilterList = items
+                } else {
+                    val resultList = ArrayList<Item>()
+                    for (row in items) {
+                        if (row.name.toLowerCase(Locale.ROOT).contains(charSearch.toLowerCase(Locale.ROOT))) {
+                            resultList.add(row)
+                        }
+                    }
+                    itemFilterList = resultList
+                }
+                val filterResults = FilterResults()
+                filterResults.values = itemFilterList
+                return filterResults
+            }
+
+            @Suppress("UNCHECKED_CAST")
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                itemFilterList = results?.values as ArrayList<Item>
+                notifyDataSetChanged()
+            }
+        }
+    }
+
     override fun onBindViewHolder(holder: ItemAdapter.ItemViewHolder, position: Int) {
-        holder.textView1.text = items[position].name;
+        holder.textView1.text = itemFilterList[position].name;
     }
 
     override fun onItemClick(view: View, position: Int) {
-        val selectedItem = items.get(position);
+        val selectedItem = itemFilterList.get(position);
         listener?.onItemClickListener(selectedItem);
     }
 
@@ -47,7 +112,7 @@ class ItemAdapter(context: Context, items: List<Item>) :
 
 
     override fun getItemCount(): Int {
-        return items.size;
+        return itemFilterList.size;
     }
 
 
--- app/src/main/java/pl/chemik/ubiapp/activities/ItemsListActivity.kt
+++ app/src/main/java/pl/chemik/ubiapp/activities/ItemsListActivity.kt
@@ -3,26 +3,41 @@ package pl.chemik.ubiapp.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import pl.chemik.ubiapp.R
 import pl.chemik.ubiapp.database.UbiApp
+import pl.chemik.ubiapp.database.entities.Box
 import pl.chemik.ubiapp.database.entities.Item
 
 class ItemsListActivity : AppCompatActivity(), RecycledListItemClickListener {
 
     var recyclerView: RecyclerView? = null;
-    var items: List<Item>? = null;
+    var itemAdapter: ItemAdapter? = null;
+    lateinit var spinnerBoxes: Spinner;
+    var allItems: List<Item>? = null;
+    var items: MutableList<Item>? = null;
+    var boxes: List<Box>? = null;
+    val boxNames: MutableList<String> = mutableListOf("Wszystkie pudła");
+    val boxIds: MutableList<Int> = mutableListOf(-1);
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_items_list)
         recyclerView = findViewById(R.id.recyclerItems);
-        initializeView();
+        spinnerBoxes = findViewById(R.id.spinnerItemsListBoxes);
+        setListeners();
     }
 
     override fun onRestart() {
@@ -36,21 +51,74 @@ class ItemsListActivity : AppCompatActivity(), RecycledListItemClickListener {
     }
 
     fun initializeView() {
+        findViewById<EditText>(R.id.etSearchItems).setText("")
         loadItemsFromDatabase();
+        loadBoxesFromDatabase();
         while (items == null) {
             Log.d("WAITING", "Waiting for get items")
         }
-        val itemAdapter = ItemAdapter(this, items!!);
+        itemAdapter = ItemAdapter(this, items!!);
         recyclerView?.adapter = itemAdapter;
         recyclerView?.layoutManager = LinearLayoutManager(this);
-        itemAdapter.setItemClickListener(this);
+        itemAdapter!!.setItemClickListener(this);
+        while (boxes == null) {
+            Log.d("WAITING", "Waiting for get boxes")
+        }
+        for (box in boxes!!) {
+            if (boxIds.contains(box.id)) {
+                continue
+            }
+            boxIds.add(box.id)
+            boxNames.add(box.name)
+        }
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, boxNames!!)
+        spinnerBoxes.adapter = adapter;
+        spinnerBoxes.setSelection(0);
+    }
+
+    fun setListeners() {
+        spinnerBoxes.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                onClickSpinnerBoxes(view, position, id);
+            }
+        }
+        findViewById<EditText>(R.id.etSearchItems).addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                itemAdapter?.filter?.filter(s.toString());
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        });
     }
 
     fun loadItemsFromDatabase() {
         GlobalScope.launch {
             val itemDao = UbiApp.database?.itemDao();
-            items = itemDao?.getAll();
+            allItems = itemDao?.getAll();
+            items = mutableListOf();
+            allItems?.let { items!!.addAll(it) }
+
         }
+    }
+
+    fun loadBoxesFromDatabase() {
+        GlobalScope.launch {
+            val boxDao = UbiApp.database?.boxDao();
+            boxes = boxDao?.getAll();
+        }
+    }
+
+    fun onClickSpinnerBoxes(view: View?, position: Int, id: Long) {
+        val boxId = boxIds[position];
+        findViewById<EditText>(R.id.etSearchItems).setText("");
+        itemAdapter?.filterByBox(boxId)?.filter("");
+
     }
 
     fun onAddClick(view: View) {
